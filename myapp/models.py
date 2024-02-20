0edfe27463507e2a42dfb90ba67e382eb189d765
Vishnu <vishnushaji179@gmail.com>
2024-02-20 15:59:45 +0530

    commit

--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from gdstorage.storage import GoogleDriveStorage
+
+# Define Google Drive Storage
+gd_storage = GoogleDriveStorage()
 
 class MyModel(models.Model):
     char_field = models.CharField(max_length=100)
     text_field = models.TextField()
-    file_field = models.FileField(upload_to='files/')
-    image_field = models.ImageField(upload_to='images/')
+    file_field = models.FileField(upload_to='files/', storage=gd_storage)  # Use Google Drive storage for file_field
+    image_field = models.ImageField(upload_to='images/', storage=gd_storage)  # Use Google Drive storage for image_field
--- myproject/settings.py
+++ myproject/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gdstorage',
     'rest_framework',
     'myapp', 
 ]
@@ -124,3 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE = 'service_key.json'  # Path to your service account key file in the root folder
+GOOGLE_DRIVE_STORAGE_MEDIA_ROOT = '/uploads'  # Base Google Drive path for file uploads
+
